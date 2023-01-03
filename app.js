92eb679c31b725b3e9412bc1ea20e7d9aa3db7ef
Fene-87 <fenekayas87@gmail.com>
2023-01-03 22:24:46 +0300

    Modify app.js file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menuList = document.querySelector('.menu-toggle');
 const menuIcon = document.querySelector('.menu-icon');
 const removeMenu = document.querySelector('.remove-icon');
+const featuredSpeakers = document.querySelector('.featured-speakers')
 
 menuIcon.addEventListener('click', () => {
     menuList.classList.add('active');
