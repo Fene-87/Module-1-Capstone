7f7022eb80f8a0131ec174e50b44416260b6878f
Fene-87 <fenekayas87@gmail.com>
2023-01-04 16:41:14 +0300

    Dynamically add featured speakers section

--- app.js
+++ app.js
@@ -11,6 +11,44 @@ removeMenu.addEventListener('click', () => {
     menuList.classList.remove('active');
 })
 
+const speakerInfo = [
+    {
+        name: 'Khari Turner',
+        shortDescription: 'An American Artist living and working in New York',
+        longDescription: 'Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo',
+    },
+
+    {
+        name: 'Brook Hsu',
+        shortDescription: 'A Taiwanese-American artist with recent solo exhibitions',
+        longDescription: 'Brook\'s abstract and figurative works are rife with symbolism serving as mysterious dreamlike mirrors of reality',
+    },
+
+    {
+        name: 'Haley Josephs',
+        shortDescription: 'A painter of mystical and vibrant imagery',
+        longDescription: 'Joseph\'s paintings use bold colors and dream-like imagery to explore themes of feminity and mortality',
+    },
+
+    {
+        name: 'Sarah Zapata',
+        shortDescription: 'A Peruvian-American fabric artist',
+        longDescription: 'Zapata creates technicolored handwoven sculptures that draw on Peruvian weaving techniques',
+    },
+
+    {
+        name: 'Derek Weisberg',
+        shortDescription: 'Participated in over 90 shows in the last 8 years',
+        longDescription: 'Based in Oakland Carlifonia, Derek is a figurative artist whose sculptures are viscerally human',
+    },
+
+    {
+        name: 'Elizabeth Englander',
+        shortDescription: 'An American post war and contemporary artist',
+        longDescription: 'Numerous key galleries and museums such as LOMEX have featured Elizabeth\'s work in the past',
+    }
+]
+
 featuredSpeakers.innerHTML = `
 <div class="program-div">
             <h2 class="speakers-header">Featured Speakers</h2>
@@ -27,10 +65,10 @@ featuredSpeakers.innerHTML = `
                 </div>
                 <div>
                     <article class="article-items">
-                        <h3>Khari Turner</h3>
-                        <h4>An American Artist living and working in New York</h4>
-                        <div></div>
-                        <p>Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo</p>
+                        <h3></h3>
+                        <h4></h4>
+                        <div class="speaker-line"></div>
+                        <p></p>
                     </article>
                 </div>
             </div>
@@ -45,10 +83,10 @@ featuredSpeakers.innerHTML = `
                 </div>
                 <div>
                     <article class="article-items">
-                        <h3>Brook Hsu</h3>
-                        <h4>An American Artist living and working in New York</h4>
-                        <div></div>
-                        <p>Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo</p>
+                        <h3></h3>
+                        <h4></h4>
+                        <div class="speaker-line"></div>
+                        <p></p>
                     </article>
                 </div>
             </div>
@@ -63,10 +101,10 @@ featuredSpeakers.innerHTML = `
                 </div>
                 <div>
                     <article class="article-items">
-                        <h3>Haley Josephs</h3>
-                        <h4>An American Artist living and working in New York</h4>
-                        <div></div>
-                        <p>Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo</p>
+                        <h3></h3>
+                        <h4></h4>
+                        <div class="speaker-line"></div>
+                        <p></p>
                     </article>
                 </div>
             </div>
@@ -81,10 +119,10 @@ featuredSpeakers.innerHTML = `
                 </div>
                 <div>
                     <article class="article-items">
-                        <h3>Sarah Zapata</h3>
-                        <h4>An American Artist living and working in New York</h4>
-                        <div></div>
-                        <p>Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo</p>
+                        <h3></h3>
+                        <h4></h4>
+                        <div class="speaker-line"></div>
+                        <p></p>
                     </article>
                 </div>
             </div>
@@ -99,10 +137,10 @@ featuredSpeakers.innerHTML = `
                 </div>
                 <div>
                     <article class="article-items">
-                        <h3>Derek Weisberg</h3>
-                        <h4>An American Artist living and working in New York</h4>
-                        <div></div>
-                        <p>Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo</p>
+                        <h3></h3>
+                        <h4></h4>
+                        <div class="speaker-line"></div>
+                        <p></p>
                     </article>
                 </div>
             </div>
@@ -117,10 +155,10 @@ featuredSpeakers.innerHTML = `
                 </div>
                 <div>
                     <article class="article-items">
-                        <h3>Elizabeth Englander</h3>
-                        <h4>An American Artist living and working in New York</h4>
-                        <div></div>
-                        <p>Khari is a well reknowned artist with two sold out shows at both Ross-sutton gallery and Frevo</p>
+                        <h3></h3>
+                        <h4></h4>
+                        <div class="speaker-line"></div>
+                        <p></p>
                     </article>
                 </div>
             </div>
@@ -130,7 +168,16 @@ featuredSpeakers.innerHTML = `
 `
 
 const speakerDetails = document.querySelectorAll('.speaker-details');
+const speakerHeader = document.querySelectorAll('.article-items h3');
+const speakerShortDesc = document.querySelectorAll('.article-items h4');
+const speakerLongDesc = document.querySelectorAll('.article-items p');
 const speakerButton = document.querySelector('.speakers-button');
+
+for (let i = 0; i < speakerDetails.length; i++) {
+    speakerHeader[i].textContent = speakerInfo[i].name;
+    speakerShortDesc[i].textContent = speakerInfo[i].shortDescription;
+    speakerLongDesc[i].textContent = speakerInfo[i].longDescription;
+}
 
 speakerButton.addEventListener('click', () => {
     for (let i = 2; i < speakerDetails.length; i++){
